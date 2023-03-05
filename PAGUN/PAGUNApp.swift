9051bdb6720d80af3fc872211a8ff7e128fb4b5a
Taiza <treerasun.chan@gmail.com>
2023-03-05 11:00:54 +0700

    Initial Commit

---
+++ PAGUN/PAGUNApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAGUNApp.swift
+//  PAGUN
+//
+//  Created by Treerasun Chanachok on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PAGUNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
